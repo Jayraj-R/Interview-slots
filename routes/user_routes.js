b163e9ac90fa8256f3efc2a8a889c9a15be256c6
Jayraj Rathod <jayrahod5@gmail.com>
2022-10-15 17:25:36 +0530

    feat : Added CRUD API routes for user

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllUsers } = require('../controllers/user');
+const {
+	getAllUsers,
+	createUser,
+	updateUser,
+	deleteUser,
+} = require('../controllers/user');
 
 /**
  * @route GET api/getAllUsers
@@ -9,5 +14,26 @@ const { getAllUsers } = require('../controllers/user');
  * @access public
  */
 router.get('/', getAllUsers);
+
+/**
+ * @route post api/createUser
+ * @description create a new user
+ * @access public
+ */
+router.post('/', createUser);
+
+/**
+ * @route GET api/updateUser
+ * @description UPdate an existing User
+ * @access public
+ */
+router.put('/:id', updateUser);
+
+/**
+ * @route GET api/deleteUser
+ * @description Delete the upcoming User
+ * @access public
+ */
+router.delete('/:id', deleteUser);
 
 module.exports = router;
