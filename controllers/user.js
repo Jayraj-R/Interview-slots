f0218342560b945cc56a9d772381f7c428a07942
Jayraj Rathod <jayrahod5@gmail.com>
2022-10-15 17:25:26 +0530

    feat : Added CRUD API functionality for user

--- controllers/user.js
+++ controllers/user.js
@@ -7,3 +7,32 @@ exports.getAllUsers = (req, res) => {
 			res.status(404).json({ message: 'User not found', error: err.message })
 		);
 };
+
+exports.createUser = (req, res) => {
+	User.create(req.body)
+		.then((data) => res.json({ message: 'User added successfully', data }))
+		.catch((err) =>
+			res
+				.status(400)
+				.json({ message: 'Failed to create an user', error: err.message })
+		);
+};
+
+exports.updateUser = (req, res) => {
+	// console.log(req, res, '--------------------------');
+	User.findByIdAndUpdate(req.params.id, req.body)
+		.then((data) => res.json({ message: 'updated successfully!', data }))
+		.catch((err) =>
+			res
+				.status(400)
+				.json({ message: 'Failed to update User', error: err.message })
+		);
+};
+
+exports.deleteUser = (req, res) => {
+	User.findByIdAndRemove(req.params.id, req.body)
+		.then((data) => res.json({ message: 'User deleted successfully', data }))
+		.catch((err) =>
+			res.status(404).json({ message: 'User not found', error: err.message })
+		);
+};
